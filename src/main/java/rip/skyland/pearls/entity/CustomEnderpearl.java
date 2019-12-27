b87cc922c1ebdf8d72a074f6b58b179ab5568510
NotNV6 <patricktiborz@gmail.com>
2019-12-27 03:22:28 +0100

    moved particle effect

--- src/main/java/rip/skyland/pearls/entity/CustomEnderpearl.java
+++ src/main/java/rip/skyland/pearls/entity/CustomEnderpearl.java
@@ -47,7 +47,6 @@ public class CustomEnderpearl extends EntityEnderPearl {
             return;
         }
 
-        IntStream.range(0, 32).forEach(i -> this.world.addParticle("portal", this.locX, this.locY + this.random.nextDouble() * 2.0D, this.locZ, this.random.nextGaussian(), 0.0D, this.random.nextGaussian()));
 
         if (this.world.isStatic) {
             return;
@@ -113,6 +112,8 @@ public class CustomEnderpearl extends EntityEnderPearl {
                 }
 
                 entityplayer.playerConnection.teleport(event.getTo());
+                IntStream.range(0, 32).forEach(i -> this.world.addParticle("portal", this.locX, this.locY + this.random.nextDouble() * 2.0D, this.locZ, this.random.nextGaussian(), 0.0D, this.random.nextGaussian()));
+                
                 this.getShooter().fallDistance = 0.0F;
 
                 if(Locale.PEARL_DAMAGE.getAsBoolean()) {
