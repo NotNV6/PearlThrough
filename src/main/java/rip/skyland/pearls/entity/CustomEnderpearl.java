2a2a4ec53970824c46c57da4e8a382392c71942f
SrPandaStick <oscardali370@gmail.com>
2020-07-24 19:00:07 -0500

    Bug fix with blocks above the fences and now it works with all types of block

--- src/main/java/rip/skyland/pearls/entity/CustomEnderpearl.java
+++ src/main/java/rip/skyland/pearls/entity/CustomEnderpearl.java
@@ -1,5 +1,6 @@
 package rip.skyland.pearls.entity;
 
+import lombok.Getter;
 import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -10,6 +11,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import rip.skyland.pearls.Locale;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 // TODO: clean code
@@ -18,22 +21,42 @@ public class CustomEnderpearl extends EntityEnderPearl {
     private boolean passedThroughSlabOrStair = false;
     private boolean passedThroughFence = false;
 
+    @Getter
+    private final ArrayList<Block> fenceTypes = new ArrayList<>(Arrays.asList(
+            Blocks.FENCE, Blocks.ACACIA_FENCE, Blocks.BIRCH_FENCE,
+            Blocks.DARK_OAK_FENCE, Blocks.JUNGLE_FENCE, Blocks.NETHER_BRICK_FENCE,
+            Blocks.SPRUCE_FENCE, Blocks.FENCE
+    ));
+
+    @Getter
+    private final ArrayList<Block> fenceGateTypes = new ArrayList<>(Arrays.asList(
+            Blocks.FENCE_GATE, Blocks.ACACIA_FENCE_GATE, Blocks.BIRCH_FENCE_GATE,
+            Blocks.DARK_OAK_FENCE_GATE, Blocks.JUNGLE_FENCE_GATE, Blocks.SPRUCE_FENCE_GATE
+    ));
+
     public CustomEnderpearl(Player player) {
         super(((CraftPlayer) player).getHandle().world, ((CraftPlayer) player).getHandle());
     }
 
     protected void a(MovingObjectPosition movingObjectPosition) {
-        Block block = this.world.c(movingObjectPosition.a());
+        final Block block;
+
+        //fix crash by ticking entity
+        try {
+            block = this.world.getType(movingObjectPosition.a()).getBlock();
+        } catch (Exception ex) {
+            return;
+        }
 
         // check if it's a tripwire
         if ((block.equals(Blocks.TRIPWIRE) && Locale.PEARL_THROUGH_TRIPWIRE.getAsBoolean())) return;
 
-        if((block.equals(Blocks.FENCE_GATE) && BlockFenceGate.a(block, block) && Locale.PEARL_THROUGH_OPEN_FENCE.getAsBoolean())) {
+        if((getFenceGateTypes().contains(block) && BlockFenceGate.a(block, block) && Locale.PEARL_THROUGH_OPEN_FENCE.getAsBoolean())) {
             this.passedThroughFence = true;
             return;
         }
 
-        if (block.equals(Blocks.FENCE) && Locale.PEARL_THROUGH_FENCE.getAsBoolean()) {
+        if (getFenceTypes().contains(block) && Locale.PEARL_THROUGH_FENCE.getAsBoolean()) {
             this.passedThroughFence = true;
             return;
         }
@@ -69,7 +92,6 @@ public class CustomEnderpearl extends EntityEnderPearl {
 
             // crit block for taliban pearling
             Location toCheck = getCheckableLocation(location);
-
 
             if (Locale.TALIBAN_PEARLING.getAsBoolean() && toCheck.add(player.getEyeLocation().getDirection().multiply(1.5)).getBlock().getType().isSolid() && this.passedThroughSlabOrStair) {
 
@@ -118,7 +140,7 @@ public class CustomEnderpearl extends EntityEnderPearl {
 
                 this.getShooter().fallDistance = 0.0F;
 
-                if(Locale.PEARL_DAMAGE.getAsBoolean()) {
+                if (Locale.PEARL_DAMAGE.getAsBoolean()) {
                     CraftEventFactory.entityDamage = this;
                     this.getShooter().damageEntity(DamageSource.FALL, 5.0F);
                     CraftEventFactory.entityDamage = null;
@@ -130,8 +152,8 @@ public class CustomEnderpearl extends EntityEnderPearl {
     }
 
     private Location getClosestSafeLocation(Location location1) {
-        for(int i = 0; i < 3; i++) {
-            if(!location1.add(location1.getDirection().multiply(i)).getBlock().getType().isSolid())
+        for (int i = 0; i < 3; i++) {
+            if (!location1.add(location1.getDirection().multiply(i)).getBlock().getType().isSolid())
                 return location1.add(location1.getDirection().multiply(1));
         }
         return null;
@@ -144,8 +166,8 @@ public class CustomEnderpearl extends EntityEnderPearl {
     }
 
     private boolean passableBlock(Block block, MovingObjectPosition movingObjectPosition) {
-        return  (block.equals(Blocks.TRIPWIRE) && Locale.PEARL_THROUGH_TRIPWIRE.getAsBoolean()) &&
-                (block.equals(Blocks.FENCE_GATE) && BlockFenceGate.a(block, block) && Locale.PEARL_THROUGH_OPEN_FENCE.getAsBoolean());
+        return (block.equals(Blocks.TRIPWIRE) && Locale.PEARL_THROUGH_TRIPWIRE.getAsBoolean()) &&
+                (getFenceGateTypes().contains(block) && BlockFenceGate.a(block, block) && Locale.PEARL_THROUGH_OPEN_FENCE.getAsBoolean());
     }
 
 }
