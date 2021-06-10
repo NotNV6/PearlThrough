0dddf7095a4b71e2231a2c2f16827a595f198a3b
Dali <oscardali370@gmail.com>
2021-06-10 12:00:58 -0500

    Command cleanup

--- src/main/java/rip/skyland/pearls/commands/PearlCommand.java
+++ src/main/java/rip/skyland/pearls/commands/PearlCommand.java
@@ -15,13 +15,12 @@ import java.util.List;
 // TODO: clean code
 public class PearlCommand implements CommandExecutor {
 
-    private String[] arguments = {
+    private final static String[] ARGUMENTS = {
             "taliban", "openfence", "fence", "tripwire", "slab", "stair", "antiglitch"
     };
 
-
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
         if(!sender.hasPermission("pearl.configure")) {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cNo permission."));
             return true;
@@ -32,14 +31,13 @@ public class PearlCommand implements CommandExecutor {
             return true;
         }
 
-        if (Arrays.stream(arguments).noneMatch(argument -> argument.equalsIgnoreCase(args[0])) || (!args[1].equalsIgnoreCase("false") && !args[1].equalsIgnoreCase("true"))) {
+        if (Arrays.stream(ARGUMENTS).noneMatch(argument -> argument.equalsIgnoreCase(args[0])) || (!args[1].equalsIgnoreCase("false") && !args[1].equalsIgnoreCase("true"))) {
             this.sendHelp(sender, label);
             return true;
         }
 
-        String argument = args[0].toLowerCase();
-
-        Locale locale = argument.equals("fence") ? Locale.PEARL_THROUGH_FENCE : argument.equals("taliban") ? Locale.TALIBAN_PEARLING : (argument.equals("openfence") ? Locale.PEARL_THROUGH_OPEN_FENCE : (
+        final String argument = args[0].toLowerCase();
+        final Locale locale = argument.equals("fence") ? Locale.PEARL_THROUGH_FENCE : argument.equals("taliban") ? Locale.TALIBAN_PEARLING : (argument.equals("openfence") ? Locale.PEARL_THROUGH_OPEN_FENCE : (
                 argument.equals("tripwire") ? Locale.PEARL_THROUGH_TRIPWIRE : (argument.equals("slab") ? Locale.PEARL_THROUGH_SLAB : (
                         argument.equals("stair") ? Locale.PEARL_THROUGH_STAIR : Locale.ANTI_GLITCH
                 ))
@@ -47,16 +45,13 @@ public class PearlCommand implements CommandExecutor {
 
         locale.setValue(Boolean.parseBoolean(args[1]));
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&fYou have updated " + argument + "'s value to " + args[1].toLowerCase()));
-
         return true;
     }
 
     private void sendHelp(CommandSender sender, String label) {
-        List<String> strings = new ArrayList<>(Collections.singletonList(StringUtils.repeat("&9&m-", 35)));
-
-        Arrays.stream(arguments).forEach(argument -> strings.add("&c/" + label + " " + argument + " <true|false>"));
+        final List<String> strings = new ArrayList<>(Collections.singletonList(StringUtils.repeat("&9&m-", 35)));
+        Arrays.stream(ARGUMENTS).forEach(argument -> strings.add("&c/" + label + " " + argument + " <true|false>"));
         strings.add(StringUtils.repeat("&9&m-", 35));
-
         strings.forEach(string -> sender.sendMessage(ChatColor.translateAlternateColorCodes('&', string)));
     }
 
